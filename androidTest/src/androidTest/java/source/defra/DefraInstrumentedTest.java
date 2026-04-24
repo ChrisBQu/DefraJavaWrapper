3574d3006f5f55a258e6a82d20958a6a70370cc3
Chris Quigley <chris.quigley2@gmail.com>
2026-04-24 14:19:26 -0400

    Update how error is handled in Android test

--- androidTest/src/androidTest/java/source/defra/DefraInstrumentedTest.java
+++ androidTest/src/androidTest/java/source/defra/DefraInstrumentedTest.java
@@ -11,11 +11,16 @@ public class DefraInstrumentedTest {
     public void createAndCloseNode() throws DefraException {
         DefraNodeInitOptions options = new DefraNodeInitOptions();
         options.inMemory = true;
-        System.out.println("Creating DefraNode...");
-        DefraNode node = new DefraNode(options);
-        System.out.println("DefraNode created successfully.");
-        node.close();
-        System.out.println("DefraNode closed successfully.");
-        System.out.println("Test passed.");
+        try {
+            System.out.println("Creating DefraNode...");
+            DefraNode node = new DefraNode(options);
+            System.out.println("DefraNode created successfully.");
+            node.close();
+            System.out.println("DefraNode closed successfully.");
+            System.out.println("Test passed.");
+        } catch (DefraException e) {
+            System.err.println("Test failed: " + e.getMessage());
+            throw e;
+        }
     }
 }
