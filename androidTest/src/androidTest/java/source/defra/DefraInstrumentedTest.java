764aa1b94a3f7e8d2c145e19793fbd4df27f0e67
Chris Quigley <chris.quigley2@gmail.com>
2026-04-24 14:17:24 -0400

    Make logging in tests the same

--- androidTest/src/androidTest/java/source/defra/DefraInstrumentedTest.java
+++ androidTest/src/androidTest/java/source/defra/DefraInstrumentedTest.java
@@ -11,7 +11,11 @@ public class DefraInstrumentedTest {
     public void createAndCloseNode() throws DefraException {
         DefraNodeInitOptions options = new DefraNodeInitOptions();
         options.inMemory = true;
+        System.out.println("Creating DefraNode...");
         DefraNode node = new DefraNode(options);
+        System.out.println("DefraNode created successfully.");
         node.close();
+        System.out.println("DefraNode closed successfully.");
+        System.out.println("Test passed.");
     }
 }
